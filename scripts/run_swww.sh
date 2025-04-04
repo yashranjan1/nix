302ecfd1747b2d1143e605883ebd62203b8a315f
yashranjan1 <yash.ranjan25@gmail.com>
2025-04-04 14:02:40 +0530

    fixed swww script

--- scripts/run_swww.sh
+++ scripts/run_swww.sh
@@ -1,19 +1,30 @@
-#!/bin/bash
+#!/usr/bin/env zsh
 
-# Check if a path is provided as an argument
-if [ -z "$1" ]; then
-    echo "Usage: $0 <path>"
+# Resolve commands using `which`
+WAL_BIN=$(which wal)
+SWWW_BIN=$(which swww)
+
+# Check if commands were found
+if [[ -z "$WAL_BIN" || -z "$SWWW_BIN" ]]; then
+    echo "Error: 'wal' or 'swww' not found in PATH."
+    echo "wal: $WAL_BIN"
+    echo "swww: $SWWW_BIN"
     exit 1
 fi
 
-# Assign the provided path to a variable
+# Validate wallpaper path input
+if [ -z "$1" ]; then
+    echo "Usage: $0 <path-to-wallpaper>"
+    exit 1
+fi
+
+# Variables
 path="$1"
-
-# Overwrite ~/.config/hypr/options.conf with the new wallpaper path
 config_file="$HOME/.config/hypr/options.conf"
-echo "\$wp = $path" >"$config_file"
-# Set the colorscheme using wal
-wal -i "$path"
 
-# Execute the command with the given path
-swww img "$path" --transition-type wipe --transition-fps 90 --transition-step 30
+# Update config
+echo "\$wp = $path" >"$config_file"
+
+# Set colors and wallpaper
+"$WAL_BIN" -i "$path"
+"$SWWW_BIN" img "$path" --transition-type wipe --transition-fps 90 --transition-step 30
